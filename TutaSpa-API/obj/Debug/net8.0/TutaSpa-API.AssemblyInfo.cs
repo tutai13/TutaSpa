c61e3fbe9fd362a1c56a3a099f91a34bf5e2e282
tutai13 <taidtpd10687@gmail.com>
2025-05-26 21:39:10 +0700

    test

--- TutaSpa-API/obj/Debug/net8.0/TutaSpa-API.AssemblyInfo.cs
+++ TutaSpa-API/obj/Debug/net8.0/TutaSpa-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TutaSpa-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a58a1a633b9ff22f5b9731e38d150af4501de7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TutaSpa-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TutaSpa-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
